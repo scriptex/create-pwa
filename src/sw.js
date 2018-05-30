c6530d56fb7296ca142224490bda1638a82b8036
Atanas Atanasov <scriptex.bg@gmail.com>
2018-05-30 16:24:00 +0300

    Update servie worker template

--- src/sw.js
+++ src/sw.js
@@ -2,30 +2,67 @@
  * By default the home route is cached
  */
 const routes = `[
-				'/'
-			]`;
+	'/'
+]`;
 
 /**
  * Generate a service-worker.js file
  */
 module.exports = function(name) {
-	return `self.addEventListener('install', function(event) {
+	return `const CACHE_NAME = '${name}-cache';
+const urlsToCache = ${routes};
+
+self.addEventListener('install', event => {
+	self.skipWaiting();
+
 	event.waitUntil(
-		caches.open('${name}-cache').then(function(cache) {
-			return cache.addAll(${routes});
-		})
+		caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
 	);
 });
 
-self.addEventListener('fetch', function(event) {
+self.addEventListener('fetch', event => {
 	event.respondWith(
-		caches.match(event.request).then(function(response) {
+		caches.match(event.request).then(response => {
 			if (response) {
 				return response;
 			}
 
-			return fetch(event.request);
+			const fetchRequest = event.request.clone();
+
+			return fetch(fetchRequest).then(response => {
+				if (
+					!response ||
+					response.status !== 200 ||
+					response.type !== 'basic'
+				) {
+					return response;
+				}
+
+				const responseToCache = response.clone();
+
+				event.waitUntil(
+					caches.open(CACHE_NAME).then(cache => {
+						cache.put(event.request, responseToCache);
+					})
+				);
+
+				return response;
+			});
 		})
+	);
+});
+
+self.addEventListener('activate', event => {
+	event.waitUntil(
+		caches
+			.keys()
+			.then(cacheNames =>
+				Promise.all(
+					cacheNames
+						.filter(cacheName => cacheName !== CACHE_NAME)
+						.map(cacheName => caches.delete(cacheName))
+				)
+			)
 	);
 });
 `;
